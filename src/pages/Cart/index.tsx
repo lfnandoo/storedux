dcaec0846f1ea4eeaa07cfbceeead2e549a3251a
lfnandoo <devfernandoof@gmail.com>
2021-07-17 18:05:52 -0300

    feat: add checkout button

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -3,6 +3,7 @@ import { useCallback } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { cartActions } from "../../redux/Cart.store";
 
+import { Button } from "../../components/Button";
 import { Product } from "./components/Product";
 
 import * as Styles from "./styles";
@@ -28,15 +29,20 @@ export const Cart = () => {
   return (
     <Styles.Container>
       <h1>Carrinho</h1>
-      <Styles.ProductList>
-        {productsInCart.map((item) => (
-          <Product
-            item={item}
-            onIncrementProduct={onIncrementProduct}
-            onDecrementProduct={onDecrementProduct}
-          />
-        ))}
-      </Styles.ProductList>
+      <section>
+        <Styles.ProductList>
+          {productsInCart.map((item) => (
+            <Product
+              item={item}
+              onIncrementProduct={onIncrementProduct}
+              onDecrementProduct={onDecrementProduct}
+            />
+          ))}
+        </Styles.ProductList>
+        <Styles.Checkout>
+          <Button>Fazer checkout ➡</Button>
+        </Styles.Checkout>
+      </section>
     </Styles.Container>
   );
 };
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -12,6 +12,14 @@ export const Container = styled.div`
   > h1 {
     color: var(--color-secondary);
   }
+
+  > section {
+    display: flex;
+    width: 85%;
+    margin: 2.5vh 0;
+    padding: 2.5vh 0;
+    border-top: 1px solid var(--color-white);
+  }
 `;
 
 export const ProductList = styled.ul`
@@ -19,6 +27,15 @@ export const ProductList = styled.ul`
   flex-direction: column;
   gap: 20px;
   width: 60%;
+  max-height: 60vh;
+  overflow: auto;
+`;
 
-  padding: 5vh 0;
+export const Checkout = styled.div`
+  flex: 1;
+  margin: 0 20px;
+
+  button {
+    width: 100%;
+  }
 `;
