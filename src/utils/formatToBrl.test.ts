34a0188fd29412297913c488a3d79e18b8303848
lfnandoo <devfernandoof@gmail.com>
2021-07-18 00:52:15 -0300

    test: format to brl function

---
+++ src/utils/formatToBrl.test.ts
@@ -0,0 +1,10 @@
+import formatToBrl from './formatToBrl';
+
+describe("formatToBrl", () => {
+  it("should format correctly", () => {
+    const price = 300
+    const formatedPrice = formatToBrl(price);
+
+    expect(formatedPrice).toBeTruthy();
+  });
+});
